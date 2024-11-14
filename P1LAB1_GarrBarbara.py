26c6dba423dfd8baaed0dbb95b0777f21533b282
garrb0661 <garrb0661@student.faytechcc.edu>
2024-11-14 10:46:29 -0500

    Add files via upload
    Second submission, first via github

---
+++ P1LAB1_GarrBarbara.py
@@ -0,0 +1,10 @@
+ # Barbara Garr
+ # 12 November 2024
+ # P1LAB1
+ # This program allows the user to enter their first and last names. A welcome message will appear.
+
+firstname = input('Enter your first name:')
+lastname = input('Enter your last name:')
+print('Hello', firstname, lastname+'! Welcome to CTI-110')
+
+ 
